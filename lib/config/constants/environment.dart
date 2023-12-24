cd94c8f7cca349c6d4ae9baddf4b9ff402d8952b
enmanuelmag <enmanuelmag@cardor.dev>
2023-12-24 16:01:22 -0500

    Config environment

---
+++ lib/config/constants/environment.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Environment {
+  static final String movieDbKey =
+      dotenv.get('THE_MOVIEDB_KEY', fallback: 'No key found');
+
+  static final String baseUrl = dotenv.get('BASE_URL',
+      fallback: 'No base url found, please check your .env file');
+}
